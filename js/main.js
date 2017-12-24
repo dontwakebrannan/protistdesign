8475753408d779a3a32a2ca6a5cec0527b37db8b
Brannan McGill <dontwakebrannan@gmail.com>
2017-12-23 18:20:18 -0800

    fixed position navigation issues

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-(function(){
-  
+(function() {
+  var nav = document.body.querySelector('nav');
+  var main = document.querySelector('main');
+
+  // This feels so wrong, but can't use the scroll event due to the CSS parallax wrapper on the entire page
+  var timer = setInterval(function() {
+    var x = nav.getBoundingClientRect().bottom;
+    var y = main.getBoundingClientRect().top;
+    if (y <= x) {
+      nav.classList.remove('hidden');
+    } else {
+      nav.classList.add('hidden');
+    }
+  }, 200);
+
 })();
